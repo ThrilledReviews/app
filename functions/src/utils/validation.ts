22aafc23ec879f9592a9e9084384b6445d7f87d2
Frederick Johnson <fritzj96@gmail.com>
2021-01-05 21:17:07 -0600

    fixed: stupid validation

--- functions/src/utils/validation.ts
+++ functions/src/utils/validation.ts
@@ -6,7 +6,7 @@ export const validationError = toi.ValidationError;
 export const isFullName = toi.str.length(6, 25).and(toix.str.trim());
 export const isBusinessName = toi.str.length(6, 40).and(toix.str.trim());
 export const isUsername = toi.str
-  .length(3, 15)
+  .length(3, 20)
   .and(toi.str.regex(/^[a-z0-9_]+$/i))
   .and(toix.str.lowercase())
   .and(toix.str.trim());
